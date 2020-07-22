2dd9ff95674f6cc149c1300aecbc4854a7a1262d
Trace <lukaszdabrowski881@gmail.com>
2020-07-22 11:52:59 +0200

    add player name normalization utility method

---
+++ src/BarcaBot.Infrastructure/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace BarcaBot.Infrastructure
+{
+    public static class Utils
+    {
+        public static string NormalizePlayerName(string s)
+            => string.Concat(s.Normalize(NormalizationForm.FormD).Where(
+                c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark));
+    }
+}
