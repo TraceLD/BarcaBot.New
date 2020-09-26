216e86baee209327877ee591c71021b5e12fe443
Trace <lukaszdabrowski881@gmail.com>
2020-09-26 16:04:54 +0100

    add plotly to config

---
+++ src/BarcaBot.Core/Models/Settings/PlotlySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BarcaBot.Core.Models.Settings
+{
+    public class PlotlySettings
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
